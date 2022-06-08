ce4776dfbe24d154cf1d7ed5a1734121f41b7a44
Nick De Luca <nfadeluca@gmail.com>
2022-06-08 13:05:18 -0400

    React from application.js doesn't seem to be recognizing div inside index.html.erb as root

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Entry point for the build script in your package.json
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import HelloMessage from './components/App';
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
